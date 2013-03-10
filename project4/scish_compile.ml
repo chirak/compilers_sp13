ab4e87ead473ed38c75af3bcaaede90a091b1f14
Chirag Khatri <ckhatri4@gmail.com>
2013-03-10 03:00:13 -0400

    A fully working compiler (I THINK)

--- project4/scish_compile.ml
+++ project4/scish_compile.ml
@@ -7,7 +7,7 @@ let error s = (print_string s; print_string "\n"; raise Error);;
 (* generate fresh variables *)
 let var_counter = ref 0;;
 let new_int() = (var_counter := (!var_counter) + 1; !var_counter);;
-let new_var() = "v_" ^ (string_of_int (new_int()));;
+let new_var prefix = (prefix) ^ (string_of_int (new_int()));;
 
 let compile_exp (e:Scish_ast.exp) : program = 
   let fun_env : funcsig list ref = ref [] in
@@ -26,47 +26,49 @@ let compile_exp (e:Scish_ast.exp) : program =
   in
   
   let rec compile' (expr:Scish_ast.exp) (var:string) (env:(string*int) list) : Cish_ast.stmt =
-      let _let_ (var:string) (s:stmt) =
-        (Let (var, (Int(0), 0), s), 0)
-      in
-
-    let seq3 s1 s2 s3 =
-      (Seq (s1, (Seq(s2, s3), 0)), 0)
+    let _exp (e:exp) : stmt =
+      (Exp e, 0)
+    in
+    let _let (var:string) (s:stmt) =
+      (Let (var, (Int(0), 0), s), 0)
+    in
+    let rec _seq (stmts:stmt list) : stmt = 
+      match stmts with
+          [s1;s2] -> (Seq (s1, s2), 0)
+        | hd::tl  -> (Seq(hd, (_seq tl)), 0)
+        | _ -> error "Sequence requires at least 2 stmts"
     in
 
-    let seq5 s1 s2 s3 s4 s5 =
-      (Seq (s1, (Seq(s2, (Seq (s3, (Seq (s4, s5), 0)), 0)), 0)), 0)
-    in
-
-    let seq6 s1 s2 s3 s4 s5 s6 =
-      (Seq (s1, (Seq(s2, (Seq (s3, (Seq (s4, (Seq (s5, s6), 0)), 0)), 0)), 0)), 0)
-    in
+    (* Shorthand bindings for CISH exps *)
+    let _int (i:int) : exp = (Int i, 0) in
+    let _var (v:string) : exp = (Var v, 0) in
+    let _binop (v1:exp) op (v2:exp) : exp = (Binop(v1, op, v2), 0) in
+    let _assign (v:var) (e:exp) : exp = (Assign (v, e), 0) in
+    let _call (f:exp) (args:exp list) : exp = (Call (f, args), 0) in
+    let _load (src:exp) : exp = (Load src, 0) in
+    let _store (dest:exp) (v:exp) : exp = (Store(dest, v), 0) in
+    let _malloc (size:int) : exp = (Malloc (Int(size), 0), 0) in
 
     let compile_prim op e1 e2 =
-      let _binop_ v1 op v2 = 
-        (Binop((Var v1, 0), op, (Var v2, 0)), 0)
-      in
-
-      let t1 = new_var() in
-      let t2 = new_var() in
+      let t1 = new_var "t" in
+      let t2 = new_var "t" in
       let v1 = compile' e1 t1 env in
       let v2 = compile' e2 t2 env in
-      let res = (Exp (Assign (var, (_binop_ t1 op t2)), 0),0) in
-      let s = seq3 v1 v2 res in
-        (_let_ t1 (_let_ t2 s))
+      let res = (Exp (_assign var (_binop (_var t1) op (_var t2))), 0) in
+      let s = _seq [v1;v2;res] in
+        (_let t1 (_let t2 s))
     in
 
     let compile_cons fst snd v =
-
-      let t1 = new_var() in
-      let t2 = new_var() in
+      let t1 = new_var "t" in
+      let t2 = new_var "t" in
       let v1 = compile' fst t1 env in
       let v2 = compile' snd t2 env in
-      let malloc = (Assign (v, (Malloc (Int(8), 0), 0)), 0) in
-      let store1 = (Store((Var(v), 0), (Var t1, 0)), 0) in
-      let store2 = (Store((Binop((Var(v), 0), Plus, (Int(4), 0)), 0),(Var t2, 0)),0) in
-      let s = (seq5 v1 v2 (Exp malloc, 0) (Exp store1, 0) (Exp store2, 0)) in
-        (_let_ t1 (_let_ t2 s))
+      let malloc = _assign v (_malloc 8) in
+      let store1 = _store (_var v) (_var t1) in
+      let store2 = _store (_binop (_var v) Plus (_int 4)) (_var t2) in
+      let s = _seq [v1;v2;(_exp malloc);(_exp store1);(_exp store2)] in
+        (_let t1 (_let t2 s))
     in
 
     let insert v env =
@@ -98,7 +100,7 @@ let compile_exp (e:Scish_ast.exp) : program =
     in
 
     match expr with
-        Scish_ast.Int(i) -> (Exp (Assign (var, (Int(i), 0)), 0), 0)
+        Scish_ast.Int(i) -> _exp (_assign var (_int i))
       | Scish_ast.Var(v) ->
           if in_env v env then
             let off = (List.length env) - (s_lookup v env) in
@@ -117,44 +119,50 @@ let compile_exp (e:Scish_ast.exp) : program =
              | (Scish_ast.Cons,[hd;tl])  -> compile_cons hd tl var
              | (Scish_ast.Fst,[e]) ->
                  let s = compile' e var env in 
-                 let load = (Assign(var, (Load (Var(var), 0), 0)), 0) in
-                   (Seq(s, (Exp load, 0)), 0)
+                 let load = _assign var (_load (_var var)) in
+                   _seq [s;(_exp load)]
              | (Scish_ast.Snd,[e]) ->
                  let s = compile' e var env in
-                 let load = 
-                   (Assign(var,
-                           (Load (Binop((Var(var), 0),
-                                        Plus,
-                                        (Int (4), 0)), 0), 0)), 0)
+                 let load =
+                   _assign var (_load (_binop (_var var) Plus (_int 4)))
                  in
-                   (Seq(s, (Exp load, 0)), 0)
+                   _seq [s;(_exp load)]
              | _ -> error "PrimApp applied with wrong # of args")
       | Scish_ast.Lambda(v,e) ->
-          let t = new_var() in
+          let t = new_var "f" in
           let body = compile' e "result" (insert v env) in
           let _ = mk_fun t body ["dynenv"] ["result"] in
-            compile_cons (Scish_ast.Var t) (Scish_ast.Var "dynenv") var
+          let malloc = _assign var (_malloc 8) in
+          let store1 = _store (_var var) (_var t) in
+          let store2 = _store (_binop (_var var) Plus (_int 4)) (_var "dynenv") in
+            _seq [(_exp malloc);(_exp store1);(_exp store2)]
       | Scish_ast.App(e1,e2) ->
           let func = compile' e1 var env in
-          let t1 = new_var() in
-          let t2 = new_var() in
+          let t1 = new_var "t" in
+          let t2 = new_var "t" in
           (* s1 contains function *)
-          let s1 = (Assign(t1, (Load (Var(var), 0), 0)), 0) in
+          let s1 = _assign t1 (_load (_var var)) in
           (* s2 contains dynenv *)
-          let s2 = (Assign(t2,
-                           (Load (Binop((Var(var), 0),
-                                        Plus,
-                                        (Int (4), 0)), 0), 0)), 0)
-          in
-
-          let arg_name = new_var() in
+          let s2 = _assign t2 (_load (_binop (_var var) Plus (_int 4))) in
+          let arg_name = new_var "t" in
           let arg = compile' e2 arg_name env in
-          let arg_env = compile_cons (Scish_ast.Var arg_name) (Scish_ast.Var t2) "result" in
-          let fun_call = (Exp (Assign (var, (Call ((Var t1, 0), [(Var var, 0)]), 0)), 0), 0) in
-          (*s1, s2, arg, arg_env, fun_call *)
-          let seq = seq6 func (Exp s1, 0) (Exp s2, 0) arg arg_env fun_call in
-            (_let_ t1 (_let_ t2 (_let_ arg_name seq)))
-      | Scish_ast.If(test,then_branch, else_branch) -> raise Unimplemented
+          let dynenv1 = _store (_var var) (_var arg_name) in
+          let dynenv2 = _store (_binop (_var var) Plus (_int 4)) (_var t2) in
+          let fun_call = _assign var (_call (_var t1) [(_var var)]) in
+          let s = _seq [func;(_exp s1);
+                        (_exp s2);arg;
+                        (_exp dynenv1);
+                        (_exp dynenv2);
+                        (_exp fun_call)] 
+          in
+            (_let t1 (_let t2 (_let arg_name s)))
+      | Scish_ast.If(test,then_branch, else_branch) ->
+          let t_name = new_var "t" in
+          let test_exp = compile' test t_name env in
+          let then_stmt = compile' then_branch var env in
+          let else_stmt = compile' else_branch var env in
+          let s = _seq [test_exp;(If (_var t_name, then_stmt, else_stmt), 0)] in
+            _let t_name s
   in
 
     let main_stmt = compile' e "result" [] in
