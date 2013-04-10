a6aa4c7a664c48b236d5e925427fcce454b8a994
Stephen Elliott <steell1890@gmail.com>
2013-04-09 20:07:46 -0400

    Changed interference graph to list of edges

--- ps7/interfere_graph.ml
+++ ps7/interfere_graph.ml
@@ -1,47 +1,44 @@
 open Cfg_ast
 open Cfg
 
-module OpMap = 
-  Map.Make(struct let compare = Pervasives.compare type t = operand end)
-type interfere_graph = VarSet.t OpMap.t;;
+type iGraphEdge = (operand * operand)
+
+let compare_edge (o1a, o1b) (o2a, o2b) =
+  if o1a = o2a || o1a = o2b && o1b = o2a || o1b = o2b then 0 else Pervasives.compare (o1a, o1b) (o2a, o2b)
+
+module IGraphEdgeSet = 
+  Set.Make(struct let compare = compare_edge type t = iGraphEdge end)
+
+type interfere_graph = IGraphEdgeSet.t
+
 let print_graph (i : interfere_graph) =
-  OpMap.iter (fun k v ->
-                    let nodes = vs2string v in
-                      Printf.printf "%s -> %s\n" (op2string k) nodes) i
+  IGraphEdgeSet.iter 
+    (fun (o1, o2) -> Printf.printf "%s <--> %s\n" (op2string o1) (op2string o2))
+    i
 
 (* given a function (i.e., list of basic blocks), construct the
  * interference graph for that function.  This will require that
  * you build a dataflow analysis for calculating what set of variables
  * are live-in and live-out for each program point. *)
 let build_interfere_graph (cfg : cfg) : interfere_graph =
+  
   let rec fold_insts (live_set : VarSet.t) (g : interfere_graph) = function
     | { i = _; igen_set = gen; ikill_set = kill; }::tl ->
         let live' = VarSet.diff live_set kill in
         (* Add edges from live_set nodes to gen set nodes *)
         let g' =
           VarSet.fold
-            (fun x a -> 
-               let connectors = OpMap.find x a in
-               let connectors' = VarSet.union gen connectors in
-                 OpMap.add x connectors' a)
+            (fun var -> 
+              VarSet.fold
+                (fun var' -> IGraphEdgeSet.add (var, var'))
+                gen)
             live_set
             g
         in
-        (* Add edges from gen set nodes to live_set nodes *)
-        let g'' = 
-          VarSet.fold 
-            (fun x a -> 
-               if OpMap.mem x a then
-                 OpMap.add x (VarSet.union (OpMap.find x a) live_set) a
-               else
-                 OpMap.add x live_set a)
-            gen
-            g'
-        in
-      (* Add gen set to live set *)
+        (* Add gen set to live set *)
         let live'' = VarSet.union gen live' in
           (* Recur *)
-          fold_insts live'' g'' tl
+          fold_insts live'' g' tl
     | [] -> g
   in
   
@@ -49,17 +46,15 @@ let build_interfere_graph (cfg : cfg) : interfere_graph =
     let live_set = b.live_out in        
     let g' = 
       VarSet.fold 
-        (fun var new_graph -> 
-          OpMap.add 
-            var
-            (VarSet.union
-              (if OpMap.mem var g then OpMap.find var g else empty_set)
-              (VarSet.diff live_set (single var)))
-            new_graph) live_set g
+        (fun var -> 
+          let other_nodes = VarSet.diff live_set (single var) in
+            VarSet.fold
+              (fun var' -> IGraphEdgeSet.add (var, var'))
+              other_nodes)
+        live_set
+        g
     in
       fold_insts live_set g' (List.rev b.gen_kill_sets.insts)
   in
-  
-  StringMap.fold (fun _ -> inter_build_block) cfg OpMap.empty
-;;
+    StringMap.fold (fun _ -> inter_build_block) cfg IGraphEdgeSet.empty
 
