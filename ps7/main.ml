348362119219a14d9ab91b67e8621d0d7e955e83
Chirag Khatri <ckhatri4@gmail.com>
2013-04-25 15:42:27 -0400

    Changes for main

--- ps7/main.ml
+++ ps7/main.ml
@@ -19,14 +19,16 @@ let _ =
   let prog = parse_file() in
   let cfg_prog = List.map Cfg_ast.fn2blocks prog in
   let reg_alloc_prog = List.map Reg_alloc.reg_alloc cfg_prog in
-  let mips_prog = List.concat (List.map Reg_alloc.cfg_to_mips reg_alloc_prog) in
-  let strs = List.fold_left (fun a x -> a ^ (Mips.inst2string x) ^ "\n") "" mips_prog in
-    print_string strs;
+    print_string (Cfg_ast.prog2string cfg_prog);
+    print_string (Cfg_ast.prog2string reg_alloc_prog);
+  (* let mips_prog = List.concat (List.map Reg_alloc.cfg_to_mips reg_alloc_prog) in *)
+  (* let strs = List.fold_left (fun a x -> a ^ (Mips.inst2string x) ^ "\n") "" mips_prog in *)
+  (*   print_string strs; *)
 
-  (* let cfgs = List.map build_cfg cfg_functions in *)
-  (* let igraphs = List.map build_interfere_graph cfgs in *)
-  (*   print_string (Cfg_ast.prog2string cfg_functions); *)
-  (*   List.iter print_cfg cfgs; *)
-  (*   print_string "\n\n---------------------\n\n"; *)
-  (*   List.iter (fun g -> print_graph g; print_string "\n\n";) igraphs; *)
+  (* (* let cfgs = List.map build_cfg cfg_functions in *) *)
+  (* (* let igraphs = List.map build_interfere_graph cfgs in *) *)
+  (* (*   print_string (Cfg_ast.prog2string cfg_functions); *) *)
+  (* (*   List.iter print_cfg cfgs; *) *)
+  (* (*   print_string "\n\n---------------------\n\n"; *) *)
+  (* (*   List.iter (fun g -> print_graph g; print_string "\n\n";) igraphs; *) *)
 
